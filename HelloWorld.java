a42268f4136effa1cfa037150646e70720c727ec
GangadharSonti <41698621+GangadharSonti@users.noreply.github.com>
2018-07-27 17:49:08 +0530

    Added Comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,6 @@
+// All keywords are in lower case
+// Class name starts with upper case and every subsequent word in the class name start with upper case
+// File name should be same as the class name
 public class HelloWorld{
   public static void main(String []args){
     System.out.println("Hello World");
